ac7efde216492138d19f1a20c2c08fb68a698c93
sps014 <shivendrapsingh014@gmail.com>
2019-11-22 14:00:44 +0530

    test

---
+++ dataset.js
@@ -0,0 +1,20 @@
+let piyush=
+{
+    name:"Piyush",
+    instn:"Pari Chowk",
+    outstn:"Botanical",
+    cost:"50₹",
+    dateTime:"22:11:2019 1:50pm",
+    uid:"****124554"
+}
+
+let none=
+{
+    name:"",
+    instn:"",
+    outstn:"",
+    cost:"",
+    dateTime:"",
+    uid:""
+}
+
